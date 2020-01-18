69bee565fcdf61bb38e0e9f8afd1de379075cd98
Christine Bumpous <bumpouce@gmail.com>
2020-01-18 01:05:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,7 @@ def square_array(array)
   count = 0
   
   while array[count] do
-    print array[count]
     array[count] = array[count] * array[count]
-    print array[count]
     count += 1
   end
 end
